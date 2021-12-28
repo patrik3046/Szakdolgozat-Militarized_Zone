642c75896b936da5f763e3e76eef1fade7939ae7
Kószó Patrick <h984004@stud.u-szeged.hu>
2021-12-28 18:07:46 +0100

    7 - Menu scene added

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,30 +1,24 @@
-package Controllers;
+package Main;
 
-import Controllers.FxmlController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("../Scenes/RootScene.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../Scenes/MenuScene.fxml"));
         primaryStage.setTitle("Militarized Zone");
-        primaryStage.setScene(new Scene(root, 640   , 480));
-
-
-        FXMLLoader loader = new FXMLLoader(this.getClass().getResource("RootScene.fxml"));
-        Controller controller = loader.getController();
-        controller.setMenuScene();
-
+        primaryStage.setScene(new Scene(root, 1280   , 720));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
-
-    public static void main(String[] args) {
+    public static void main(String[] args){
         launch(args);
     }
 }
