03a5df90e6ed8ff29d9fea5e5b0476ef1c9eb771
Kószó Patrick <h984004@stud.u-szeged.hu>
2022-01-23 22:25:29 +0100

    19 - Settings controller finished

--- src/controllers/SettingsController.java
+++ src/controllers/SettingsController.java
@@ -1,12 +1,18 @@
 package controllers;
 
+import elements.Bomb;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 
+import javafx.scene.Node;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 import values.globals.GlobalSettings;
 
 import java.io.IOException;
@@ -29,7 +35,19 @@ public class SettingsController extends MenuController {
     private Slider obstructionsProbabilitySlider;
 
     @FXML
-    private CheckBox costumeBombCheckBox;
+    private CheckBox customBombCheckBox;
+
+    @FXML
+    private AnchorPane customBombSettingsPane;
+
+    @FXML
+    private GridPane customBombGridPane;
+
+    @FXML
+    private Slider customObstructionsProbabilitySlider;
+
+    @FXML
+    private Label remainingTiles;
 
     private ObservableList<String> languageList = FXCollections.observableArrayList("Magyar","Angol");
 
@@ -45,19 +63,78 @@ public class SettingsController extends MenuController {
 
         obstructionsProbabilitySlider.setValue(globalSettings.getObstructionsProbability());
 
-        costumeBombCheckBox.setSelected(globalSettings.getUsingCostumeBomb());
+        customBombCheckBox.setSelected(globalSettings.getUsingCustomBomb());
+
+        if (globalSettings.getUsingCustomBomb()) {
+            remainingTiles.setText("0");
+            Bomb customBomb = globalSettings.getCustomBomb();
+            customObstructionsProbabilitySlider.setValue(customBomb.getObstructionsProbability());
+            for (int i = 0; i < customBomb.getDispersion().length; i++) {
+                for (int j = 0; j < customBomb.getDispersion().length; j++) {
+                    if (customBomb.getDispersion()[i][j] == 1) {
+                        customBombGridPane.getChildren().get(i * 5 + j).setStyle("-fx-background-color: black;");
+                    }
+                }
+            }
+        }
 
         languageComboBox.setValue(globalSettings.getLanguage());
         languageComboBox.setItems(languageList);
 
         mapSizeComboBox.setValue(globalSettings.getMapSize());
         mapSizeComboBox.setItems(mapSizeList);
+
+        checkCostumeBombState();
     }
 
     public void saveSettings() throws IOException {
-        globalSettings.setSettings(gameVolumeSlider.getValue(),musicVolumeSlider.getValue(),obstructionsProbabilitySlider.getValue(),costumeBombCheckBox.isSelected(),mapSizeComboBox.getValue(),languageComboBox.getValue());
+        Bomb bomb = new Bomb(new int[][]{{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0}},0.5);
+        if (Integer.parseInt(remainingTiles.getText()) == 0) {
+            int[][] costumBomb = new int[5][5];
+
+            for (int i = 0; i <= 24; i++) {
+                Node pane = customBombGridPane.getChildren().get(i);
+                if (pane.getStyle().equals("-fx-background-color: white;")) {
+                    costumBomb[i / 5][i % 5] = 0;
+                } else if (pane.getStyle().equals("-fx-background-color: black;")) {
+                    costumBomb[i / 5][i % 5] = 1;
+                }
+            }
+
+            bomb = new Bomb(costumBomb, customObstructionsProbabilitySlider.getValue());
+        }
+
+        globalSettings.setSettings(gameVolumeSlider.getValue(), musicVolumeSlider.getValue(), obstructionsProbabilitySlider.getValue(),(customBombCheckBox.isSelected() && Integer.parseInt(remainingTiles.getText()) == 0) ? true : false,mapSizeComboBox.getValue(),languageComboBox.getValue(),bomb);
 
         loadMenuScene();
+    }
+
+    public void checkCostumeBombState(){
+        if (customBombCheckBox.isSelected()){
+            customBombSettingsPane.setDisable(false);
+        } else {
+            customBombSettingsPane.setDisable(true);
+        }
+    }
+
+    public void costumeBombGridPaneClicked(MouseEvent event) {
+        Node clickedNode = event.getPickResult().getIntersectedNode();
+        int index = 0;
+        if (clickedNode != customBombGridPane) {
+            index = GridPane.getRowIndex(clickedNode) != null ?  5 * GridPane.getRowIndex(clickedNode) : 0;
+            index += GridPane.getColumnIndex(clickedNode) != null ? GridPane.getColumnIndex(clickedNode) : 0;
+
+            Node selectedNode = customBombGridPane.getChildren().get(index);
+
+
+            if(selectedNode.getStyle().equals("-fx-background-color: white;") && Integer.parseInt(remainingTiles.getText()) > 0){
+                customBombGridPane.getChildren().get(index).setStyle("-fx-background-color: black;");
+                remainingTiles.setText((Integer.parseInt(remainingTiles.getText()) - 1) + "");
+            } else if(selectedNode.getStyle().equals("-fx-background-color: black;")){
+                customBombGridPane.getChildren().get(index).setStyle("-fx-background-color: white;");
+                remainingTiles.setText((Integer.parseInt(remainingTiles.getText()) + 1) + "");
+            }
+        }
     }
 }
 
